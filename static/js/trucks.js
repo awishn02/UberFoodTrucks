cace7968410c20ae932b712c846be36af3ca5341
Aaron Wishnick <Aaron_B.Wishnick@tufts.edu>
2013-10-17 20:25:19 -0400

    added search by food

--- static/js/trucks.js
+++ static/js/trucks.js
@@ -1,104 +1,119 @@
 $(function() {
-	// Food truck model
+	"use strict";
 	var curPos;
+
+	// Food Truck Model //
 	var truck = Backbone.Model.extend({
-		initialize: function(){
-		}
+		initialize: function() {}
 	});
 
+	// Food Truck View //
 	var TruckView = Backbone.View.extend({
 		tagName: "li",
-    	template: _.template($('#truck-template').html()),
-    	events: {
-    		"click .view" : "center"
-    	},
-	    render: function() {
-	    	distance = this.model['dis'];
-			if (distance < .1) {
+		template: _.template($("#truck-template").html()),
+		events: {
+			"click .view": "center"
+		},
+		render: function() {
+			var distance = this.model.dis;
+			if (distance < 0.1) {
 				distance *= 5280;
 				distance = distance.toPrecision(3) + " ft";
 			} else {
 				distance = distance.toPrecision(2) + " mi";
 			}
-			this.model['dis'] = distance;
-	      	$(this.el).html(this.template(this.model));
-	      	return this;
-	    },
-	    center: function(){
-	    	$("#trucks").removeClass('show');	
-	    	destination = new google.maps.LatLng(this.model['obj']['loc'][1], this.model['obj']['loc'][0])
-	    	map.panTo(destination)
-	    	calcRoute(destination)
-	    },
-	    clear: function(){
-	    	this.model.destroy()
-	    }
+			this.model.dis = distance;
+			$(this.el).html(this.template(this.model));
+			return this;
+		},
+		center: function() {
+			$("#trucks").removeClass("show");
+			var destination = new google.maps.LatLng(this.model.job.loc[1], this.model.obj.loc[0]);
+			map.panTo(destination);
+			calcRoute(destination);
+		},
+		clear: function() {
+			this.model.destroy();
+		}
 	});
 
+	// Food Truck Collection //
 	var TruckList = Backbone.Collection.extend({
 		model: truck,
-		url: '/trucks'
-	})
+		url: "/trucks"
+	});
 
-	var Trucks = new TruckList;
+	var Trucks = new TruckList();
 
+	// App View //
 	var AppView = Backbone.View.extend({
 		el: $("#list-area"),
 		initialize: function() {
-      		Trucks.on('reset', this.addAll);
+			Trucks.on("reset", this.addAll);
 			getPosition();
 		},
 		log: function() {
-			initializeMap;
+			initializeMap();
 		},
 		render: function() {
 
 		},
-		addAll: function(data) {	
-			results = jQuery.parseJSON(JSON.stringify(data))[0]['results']
-			results.forEach(function(truck){
-				var view = new TruckView({model: truck});
-    	  		this.$("#truck-list").append(view.render().el);
+		addAll: function(data) {
+			var results = jQuery.parseJSON(JSON.stringify(data))[0].results;
+			results.forEach(function(truck) {
+				var view = new TruckView({
+					model: truck
+				});
+				$("#truck-list").append(view.render().el);
 				var marker = new google.maps.Marker({
 					map: map,
 					optimized: false,
-	    			animation: google.maps.Animation.DROP,
-					position: new google.maps.LatLng(truck['obj']['loc'][1], truck['obj']['loc'][0])
+					animation: google.maps.Animation.DROP,
+					position: new google.maps.LatLng(truck.obj.loc[1], truck.obj.loc[0])
 				});
 				markers.push(marker);
-				google.maps.event.addListener(marker, 'click', function() {
-					var distance = truck['dis'];
-					infowindow.setContent('<p>' + truck['obj']['applicant'] + '</p><p>' + distance + '</p>' +
-										  '<p class="items">' + truck['obj']['fooditems'] + '</p>')
+				google.maps.event.addListener(marker, "mouseover", function() {
+					var distance = truck.dis;
+					infowindow.setContent("<p>" + truck.obj.applicant + "</p><p>" + distance + "</p>" +
+						"<p class='items'>" + truck.obj.fooditems + "</p>");
 					infowindow.open(map, this);
+				});
+				google.maps.event.addListener(marker, 'mouseout', function(){
+					infowindow.close();
 				})
+				google.maps.event.addListener(marker, "click", function(){
+					calcRoute(new google.maps.LatLng(truck.obj.loc[1], truck.obj.loc[0]));
+				});
 			});
 		}
 	});
 
-	var App = new AppView;
+	var App = new AppView();
 
+	// Get User's Current Location //
 	function getPosition() {
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(function(position) {
-				initializeMap(position)
-				Trucks.fetch({url:'/trucks/'+position.coords.latitude+"/"+position.coords.longitude})
+				initializeMap(position);
+				Trucks.fetch({
+					url: "/trucks/" + position.coords.latitude + "/" + position.coords.longitude
+				});
 			});
 		}
 	}
 
 	var infowindow;
+	var iconFile;
 	var map;
 	var markers = [];
-	var latitude;
-	var longitude;
+	var person;
 
 	var directionsDisplay;
 	var directionsService = new google.maps.DirectionsService();
 
 	function initializeMap(position) {
-		var myLatlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-  		directionsDisplay = new google.maps.DirectionsRenderer();
+		var myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+		directionsDisplay = new google.maps.DirectionsRenderer();
 		curPos = myLatlng;
 		var mapOptions = {
 			zoom: 17,
@@ -106,53 +121,55 @@ $(function() {
 			mapTypeId: google.maps.MapTypeId.ROADMAP,
 			noClear: true
 		};
-		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  		directionsDisplay.setMap(map);
+		map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+		directionsDisplay.setMap(map);
 
 		var geocoder = new google.maps.Geocoder();
 
 		infowindow = new google.maps.InfoWindow({
 			content: "hold"
-		})
+		});
 
 		iconFile = "http://www.google.com/intl/en_us/mapfiles/ms/micons/blue-dot.png";
 
-		var person = new google.maps.Marker({
+		person = new google.maps.Marker({
 			map: map,
 			position: myLatlng,
 			optimized: false
-		}); 
-		markers.push(person);
+		});
 		person.setIcon(iconFile);
 	}
 
+	// Calculate Walking Directions //
 	function calcRoute(destination) {
-	  var request = {
-	      origin: curPos,
-	      destination: destination,
-	      // Note that Javascript allows us to access the constant
-	      // using square brackets and a string value as its
-	      // "property."
-	      travelMode: google.maps.TravelMode["WALKING"]
-	  };
-	  directionsService.route(request, function(response, status) {
-	    if (status == google.maps.DirectionsStatus.OK) {
-	      directionsDisplay.setDirections(response);
-	    }
-	  });
+		var request = {
+			origin: curPos,
+			destination: destination,
+			// Note that Javascript allows us to access the constant
+			// using square brackets and a string value as its
+			// "property."
+			travelMode: google.maps.TravelMode.WALKING
+		};
+		directionsService.route(request, function(response, status) {
+			if (status == google.maps.DirectionsStatus.OK) {
+				directionsDisplay.setDirections(response);
+			}
+		});
 	}
-	function removeMarkers(){
-		for(var i = 0; i < markers.length; i++){
+
+	function removeMarkers() {
+		$("#truck-list").empty();
+		for (var i = 0; i < markers.length; i++) {
 			markers[i].setMap(null);
 		}
 	}
 	var geocoder = new google.maps.Geocoder();
 	var autocomplete = new google.maps.places.Autocomplete($("#map-search")[0], {});
-	google.maps.event.addListener(autocomplete, 'place_changed', function() {
-        var place = autocomplete.getPlace();
-        lat = place['geometry']['location']['lb'];
-        lng = place['geometry']['location']['mb'];
-        removeMarkers();
+	google.maps.event.addListener(autocomplete, "place_changed", function() {
+		var place = autocomplete.getPlace();
+		var lat = place.geometry.location.lb;
+		var lng = place.geometry.location.mb;
+		removeMarkers();
 		var latlng = new google.maps.LatLng(lat, lng);
 		var marker = new google.maps.Marker({
 			map: map,
@@ -161,12 +178,31 @@ $(function() {
 		});
 		marker.setIcon(iconFile);
 		markers.push(marker);
-		Trucks.fetch({url:"/trucks/"+lat+"/"+lng})
-        map.setCenter(latlng);
-        curPos = latlng;
-    });
+		Trucks.fetch({
+			url: "/trucks/" + lat + "/" + lng
+		});
+		person.setPosition(latlng);
+		map.setCenter(latlng);
+		curPos = latlng;
+	});
 
-    $("#more").click(function(){
-    	$("#trucks").toggleClass('show');
-    })
+	$("#more").click(function() {
+		$("#trucks").toggleClass("show");
+	});
+
+	$("#filter").keypress(function(e){
+		if(e.which == 13){
+			if($(this).val() != ""){
+				removeMarkers();
+				Trucks.fetch({
+					url: "/trucks/"+curPos.lb + "/" + curPos.mb + "/" + $(this).val()
+				});
+			}else{
+				removeMarkers();
+				Trucks.fetch({
+					url: "/trucks/"+curPos.lb + "/" + curPos.mb
+				});
+			}
+		}
+	});
 });
--- trucks.py
+++ trucks.py
@@ -1,13 +1,14 @@
 import argparse
-import urllib, json
-import os
+import urllib
+import json
 
-from flask import Flask, g, jsonify, render_template, request, abort
-from pymongo import MongoClient, Connection, GEO2D
+from flask import Flask, render_template
+from pymongo import MongoClient, GEO2D
 from bson.json_util import dumps
 from bson.son import SON
 
-client = MongoClient('mongodb://awishn02:Reds0x9!@ds047478.mongolab.com:47478/heroku_app18310921')
+client = MongoClient(
+    "mongodb://test:user@ds047478.mongolab.com:47478/heroku_app18310921")
 #lient = MongoClient();
 app = Flask(__name__)
 
@@ -15,35 +16,59 @@ db = client.heroku_app18310921
 #db = client.uberdb
 food_trucks = db.food_trucks
 
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route("/trucks", methods=['GET'])
 def all_trucks():
-	return dumps(db.food_trucks.find())
-	
+    return dumps(db.food_trucks.find())
+
+
 @app.route("/trucks/<string:lat>/<string:lng>", methods=['GET'])
 def get_trucks(lat, lng):
-	distance = 3959/180
-	return dumps(db.command(SON([('geoNear', 'food_trucks'), ('maxDistance', 50), ('near', [float(lng),float(lat)]), ('limit', 100), ('distanceMultiplier', distance), ('query', {'status':'APPROVED'})])))
+    distance = 3959 / 180
+    return dumps(db.command(SON([('geoNear', 'food_trucks'),
+                                 ('maxDistance', 50),
+                                 ('near', [float(lng), float(lat)]),
+                                 ('limit', 100),
+                                 ('distanceMultiplier', distance),
+                                 ('query', {'status': 'APPROVED'})])))
+
+
+@app.route("/trucks/<string:lat>/<string:lng>/<string:food_type>", methods=['GET'])
+def search_food(lat, lng, food_type):
+    distance = 3959 / 180
+    return dumps(db.command(SON([('geoNear', 'food_trucks'),
+                                 ('maxDistance', 50),
+                                 ('near', [float(lng), float(lat)]),
+                                 ('limit', 100),
+                                 ('distanceMultiplier', distance),
+                                 ('query', {'status': 'APPROVED', 'fooditems':{'$regex':food_type, '$options': 'i'}})])))
+
 
 def dbSetup():
-	jsonurl = urllib.urlopen("https://data.sfgov.org/Permitting/Mobile-Food-Facility-Permit/rqzj-sfat.json")
-	trucks = json.loads(jsonurl.read())
-	db.food_trucks.create_index([("loc", GEO2D)])
-	for truck in trucks:
-		if truck.has_key('location'):
-			truck['loc'] = [float(truck['location']['longitude']), float(truck['location']['latitude'])]
-			food_trucks.insert(truck)
+    jsonurl = urllib.urlopen("""https://data.sfgov.org/Permitting/
+                                Mobile-Food-Facility-Permit/rqzj-sfat.json""")
+    trucks = json.loads(jsonurl.read())
+    db.food_trucks.create_index([("loc", GEO2D)])
+    for truck in trucks:
+        if 'location' in truck:
+            truck['loc'] = [
+                float(truck['location']['longitude']),
+                float(truck['location']['latitude'])
+            ]
+            food_trucks.insert(truck)
 
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Running uber backend')
-	parser.add_argument('--setup', dest='run_setup', action='store_true')
+    parser = argparse.ArgumentParser(description='Running uber backend')
+    parser.add_argument('--setup', dest='run_setup', action='store_true')
 
-	args = parser.parse_args()
-	if args.run_setup:
-		dbSetup()
-	else:
-		app.run(debug=True)
+    args = parser.parse_args()
+    if args.run_setup:
+        dbSetup()
+    else:
+        app.run(debug=True)
